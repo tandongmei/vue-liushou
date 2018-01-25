624e8bfe3b0fb2343818a02af1fca25c60eef971
tandongmei <17775205771@163.com>
2018-01-26 00:46:33 +0800

    ip

--- src/components/register/register.vue
+++ src/components/register/register.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-   <el-container style="background-image:url('static/images/common/back7.jpg');height:850px;">
+   <el-container style="background-image:url('static/images/common/back7.jpg');height:1050px;">
        <el-main style="background-color:#FFFFFF;width:400px;margin:50px 350px;border-radius:25px;">
            <div style="text-align:center;font-family:blod;font-size:35px;"><span style="font-color:red">注册</span></div>
            <el-form :model="user" :rules="rules" ref="userForm" label-width="110px" class="demo-ruleForm">
@@ -10,7 +10,10 @@
                 <el-form-item label="用户密码" prop="password">
                     <el-input  v-model.trim="user.password" suffix-icon="el-icon-edit"></el-input>
                 </el-form-item>
-                <el-form-item label="真实姓名" prop="name">
+                <el-form-item label="确认密码" prop="rePassword">
+                    <el-input  v-model.trim="user.rePassword" suffix-icon="el-icon-edit"></el-input>
+                </el-form-item>
+                <!-- <el-form-item label="真实姓名" prop="name">
                     <el-input v-model.trim="user.name" suffix-icon="el-icon-edit"></el-input>
                 </el-form-item>
                 <el-form-item label="性别" prop="gender">
@@ -18,6 +21,17 @@
                         <el-radio :label="1">男</el-radio>
                         <el-radio :label="0">女</el-radio>
                     </el-radio-group>
+                </el-form-item>
+                <el-form-item label="上传头像" prop="headImg">
+                  <el-upload
+                      class="avatar-uploader"
+                      action="http://localhost:8080/api/user/upload"
+                      :show-file-list="false"
+                      :on-success="handleAvatarSuccess"
+                      :before-upload="beforeAvatarUpload">
+                      <img v-if="user.headImg" :src="user.headImg" class="avatar">
+                      <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                  </el-upload>
                 </el-form-item>
                 <el-form-item label="年龄" prop="age">
                     <el-input v-model.trim="user.age" type="number" min="1" suffix-icon="el-icon-date"></el-input>
@@ -36,7 +50,7 @@
                 </el-form-item>
                 <el-form-item label="支付宝账号" prop="payNo">
                     <el-input v-model.trim="user.payNo" suffix-icon="el-icon-tickets" placeholder="请输入一个可收款的支付宝账号"></el-input>
-                </el-form-item>
+                </el-form-item> -->
                 <el-form-item>
                     <el-button type="primary" @click="submitForm('userForm')">注册</el-button>
                     <el-button @click="resetForm('userForm')">重置</el-button>
@@ -59,15 +73,6 @@ import {valiTel,valiEmail,valiPayNo} from './../../utils/validateUtil';
           callback();
         }
       };
-       const validateAge = (rule, value, callback) => {
-        if (!value) {
-          callback(new Error('请输入年龄'));
-        }else if(value < 1 || value > 200){
-          callback(new Error('年龄格式错误'));
-        } else {
-          callback();
-        }
-      };
       const validatePassword = (rule, value, callback) => {
         if (!value || value.length < 6) {
           callback(new Error('密码不能为空,至少6位'));
@@ -75,45 +80,64 @@ import {valiTel,valiEmail,valiPayNo} from './../../utils/validateUtil';
           callback();
         }
       };
-      const validateTel = (rule, value, callback) => {
+      const validateRePassword = (rule, value, callback) => {
         if (!value) {
-          callback(new Error('请输入电话'));
-        }else if(!valiTel(value)){
-          callback(new Error('电话格式错误'));
+          callback(new Error('请输入确认密码'));
+        }else if(value != this.user.password){
+          callback(new Error('两次密码输入不一致'));
         } else {
           callback();
         }
       };
-      const validateEmail = (rule, value, callback) => {
-        if (!value) {
-          callback(new Error('请输入邮箱'));
-        }else if(!valiEmail(value)){
-          callback(new Error('邮箱格式错误'));
-        } else {
-          callback();
-        }
-      };
-      const validatePayNo = (rule, value, callback) => {
-        if (!value) {
-          callback(new Error('请输入支付宝账号'));
-        }else if(!valiPayNo(value)){
-          callback(new Error('支付宝账号格式错误'));
-        } else {
-          callback();
-        }
-      };
+      //  const validateAge = (rule, value, callback) => {
+      //   if (!value) {
+      //     callback(new Error('请输入年龄'));
+      //   }else if(value < 1 || value > 200){
+      //     callback(new Error('年龄格式错误'));
+      //   } else {
+      //     callback();
+      //   }
+      // };
+      // const validateTel = (rule, value, callback) => {
+      //   if (!value) {
+      //     callback(new Error('请输入电话'));
+      //   }else if(!valiTel(value)){
+      //     callback(new Error('电话格式错误'));
+      //   } else {
+      //     callback();
+      //   }
+      // };
+      // const validateEmail = (rule, value, callback) => {
+      //   if (!value) {
+      //     callback(new Error('请输入邮箱'));
+      //   }else if(!valiEmail(value)){
+      //     callback(new Error('邮箱格式错误'));
+      //   } else {
+      //     callback();
+      //   }
+      // };
+      // const validatePayNo = (rule, value, callback) => {
+      //   if (!value) {
+      //     callback(new Error('请输入支付宝账号'));
+      //   }else if(!valiPayNo(value)){
+      //     callback(new Error('支付宝账号格式错误'));
+      //   } else {
+      //     callback();
+      //   }
+      // };
       return {
         user: {
           nickName: '',
           password: '',
-          name: '',
-          gender: 1,
-          age: '',
-          tel: '',
-          email: '',
-          isLeftChild: 1,
-          headImg: '',
-          payNo: ''
+          rePassword: ''
+          // name: '',
+          // gender: 1,
+          // age: '',
+          // tel: '',
+          // email: '',
+          // isLeftChild: 1,
+          // headImg: '',
+          // payNo: ''
         },
         rules: {
           nickName: [
@@ -122,21 +146,27 @@ import {valiTel,valiEmail,valiPayNo} from './../../utils/validateUtil';
           password: [
             { required: true, trigger: 'blur', validator: validatePassword }
           ],
-          name: [
-            { required: true, message: '请输入真实姓名', trigger: 'blur' }
-          ],
-          age: [
-            { required: true, trigger: 'blur', validator: validateAge }
-          ],
-          tel: [
-            { required: true, trigger: 'blur', validator: validateTel }
-          ],
-          email: [
-            { required: true, trigger: 'blur', validator: validateEmail }
-          ],
-          payNo: [
-            { required: true, trigger: 'blur', validator: validatePayNo }
+          rePassword: [
+            { required: true, trigger: 'blur', validator: validateRePassword }
           ]
+          // name: [
+          //   { required: true, message: '请输入真实姓名', trigger: 'blur' }
+          // ],
+          // headImg: [
+          //   { required: true, message: '请上传头像', trigger: 'blur' }
+          // ],
+          // age: [
+          //   { required: true, trigger: 'blur', validator: validateAge }
+          // ],
+          // tel: [
+          //   { required: true, trigger: 'blur', validator: validateTel }
+          // ],
+          // email: [
+          //   { required: true, trigger: 'blur', validator: validateEmail }
+          // ],
+          // payNo: [
+          //   { required: true, trigger: 'blur', validator: validatePayNo }
+          // ]
           
         }
       };
@@ -167,12 +197,48 @@ import {valiTel,valiEmail,valiPayNo} from './../../utils/validateUtil';
       },
       resetForm(formName) {
         this.$refs[formName].resetFields();
+      },
+      handleAvatarSuccess(res, file) {
+        this.user.headImg = URL.createObjectURL(file.raw);
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
       }
     }
   }
 </script>
 <style>
-
+.avatar-uploader .el-upload {
+    border: 1px dashed #909399;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
 </style>
 
 
