9f1f63788435c31e1d31ea64d89f112c7f61a409
tandongmei <tandongmei@xianbeitech.com>
2018-03-12 21:59:40 +0800

    up

--- src/components/common/head.vue
+++ src/components/common/head.vue
@@ -3,6 +3,7 @@
     <el-row >
       <el-col :span="24">
       <div class="grid-content bg-purple-dark">
+        
         <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
             <el-row>
               <el-col :span="4">
