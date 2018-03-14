446d74951fe99adc1a8e9a05d2887cfbfe23a6c5
tandongmei <tandongmei@xianbeitech.com>
2018-03-14 09:06:02 +0800

    up

--- src/components/common/head.vue
+++ src/components/common/head.vue
@@ -4,40 +4,38 @@
       <el-col :span="24">
       <div class="grid-content bg-purple-dark">
         
-        <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-            <el-row>
-              <el-col :span="4">
-                <el-menu-item index="0"><span style="font-weight:bolder;font-size:35px;color:#ffd04b">LIU SHOU</span></el-menu-item>
-              </el-col>
-              <el-col :span="17">
-                <el-menu-item index="1"><router-link to="/shouye">网站首页</router-link></el-menu-item>
-                <el-submenu index="2">
-                    <template slot="title">留守之家</template>
-                    <el-menu-item index="2-1"><router-link to="/home/child">关爱留守</router-link></el-menu-item>
-                    <el-menu-item index="2-2"><router-link to="/home/help">寻求帮助</router-link></el-menu-item>
-                </el-submenu>
-                <el-submenu index="3">
-                    <template slot="title">爱心公益</template>
-                    <el-menu-item index="3-1"><router-link to="/home/loveUser">公益新闻</router-link></el-menu-item>
-                    <el-menu-item index="3-2"><router-link to="/home/assist">为他助力</router-link></el-menu-item>
-                </el-submenu>
-                <el-menu-item index="4">心灵大使</el-menu-item>
-                <el-menu-item index="5">亲子互动</el-menu-item>
-              </el-col>
-              <el-col v-if="nickName"  :span="3">
-                <el-submenu index="6">
-                    <template slot="title"><img :src="headImg" style="width:38px;height:38px;border-radius:120px;margin-right:5px">{{nickName}}</template>
-                    <el-menu-item index="6-1">我的评论<el-badge class="mark" :value="noReadCommentCount" /></el-menu-item>
-                    <el-menu-item index="6-2" >我的回复<el-badge class="mark" :value="noReadReplyCount" /></el-menu-item>
-                    <el-menu-item index="6-3"><router-link to="/update">修改资料</router-link></el-menu-item>
-                    <el-menu-item index="6-4" @click="exit">退出登陆</el-menu-item>
-                </el-submenu>
-              </el-col>
-              <el-col v-else :span="3">
-                <el-menu-item index="6"><router-link to="/login">登陆</router-link></el-menu-item>
-                <el-menu-item index="7"><router-link to="/register">注册</router-link></el-menu-item>
-              </el-col>
-            </el-row>
+        
+        <el-menu
+          :default-active="activeIndex2"
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-menu-item index="0"><span style="font-weight:bolder;font-size:35px;color:#ffd04b">LIU SHOU</span></el-menu-item>
+          <el-menu-item index="1"><router-link to="/shouye">网站首页</router-link></el-menu-item>
+          <el-submenu index="2">
+              <template slot="title">留守之家</template>
+              <el-menu-item index="2-1"><router-link to="/home/child">关爱留守</router-link></el-menu-item>
+              <el-menu-item index="2-2"><router-link to="/home/help">寻求帮助</router-link></el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+              <template slot="title">爱心公益</template>
+              <el-menu-item index="3-1"><router-link to="/home/loveUser">公益新闻</router-link></el-menu-item>
+              <el-menu-item index="3-2"><router-link to="/home/assist">为他助力</router-link></el-menu-item>
+          </el-submenu>
+          <el-menu-item index="4">心灵大使</el-menu-item>
+          <el-menu-item index="5">亲子互动</el-menu-item>
+          <el-submenu index="6" v-if="nickName">
+              <template slot="title"><img :src="headImg" style="width:38px;height:38px;border-radius:120px;margin-right:5px">{{nickName}}</template>
+              <el-menu-item index="6-1">我的评论<el-badge class="mark" :value="noReadCommentCount" /></el-menu-item>
+              <el-menu-item index="6-2" >我的回复<el-badge class="mark" :value="noReadReplyCount" /></el-menu-item>
+              <el-menu-item index="6-3"><router-link to="/update">修改资料</router-link></el-menu-item>
+              <el-menu-item index="6-4" @click="exit">退出登陆</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="6"><router-link to="/login">登陆</router-link></el-menu-item>
+          <el-menu-item index="7"><router-link to="/register">注册</router-link></el-menu-item>
         </el-menu>
       </div>
     </el-col>
@@ -88,7 +86,9 @@ export default {
 }
 </script>
 <style>
-
+.el-menu--horizontal {
+   top: 0px; 
+}
 </style>
 
 
